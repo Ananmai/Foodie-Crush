7c0d01d99bf58225a98f84e526328f033cbe4342
G N Chinmai <gnchinmaikinnal@gmail.com>
2026-05-12 20:06:25 +0530

    Enable database migrations on Vercel

--- build_files.sh
+++ build_files.sh
@@ -12,4 +12,7 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --noinput --clear
 
+# Run database migrations
+python manage.py migrate --noinput
+
 echo "BUILD END"
